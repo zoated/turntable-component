eb8a5f84de3362f2de8647805fd2d5e4ddbf5e1f
chenjiazheng <chenjiazheng@mampod.com>
2023-05-23 16:24:08 +0800

    通过打包输出docs

---
+++ website/root.tsx
@@ -0,0 +1,10 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Turntable from './turntable';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Turntable />
+  </StrictMode>
+);
